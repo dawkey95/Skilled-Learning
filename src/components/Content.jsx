feac87f20219739e3b14714b23d9c5e427cc5b59
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-04-03 22:13:25 +1200

    working on content section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import Card from "./Card";
+import data from "../data/data";
+// import Card from "./Card";
 
 const Content = () => {
   return (
     <>
       <section className="content-section">
         <div className="content-container">
-          <div className="content-header">
-            <h3 className="content-title">
-              Check out our most popular courses!
-            </h3>
-          </div>
-          <Card />
+          <h3 className="content-title">Check out our most popular courses!</h3>
+            {data.map(({ id, title, details, imageURL }) => {
+              return (
+                <div className="card-body" key={id}>
+                  <img className="card-image" src={imageURL} />
+                  <h3 className="card-title">{title}</h3>
+                  <p className="card-details">{details}</p>
+                  <button className="card-btn">Get Started</button>
+                </div>
+              );
+            })}
         </div>
       </section>
     </>
